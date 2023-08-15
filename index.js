634e1d26610b9f9bade975b46549662a9c31c3be
amyhytwn@aol.com <135174286+pmastropolo@users.noreply.github.com>
2023-08-15 16:32:23 -0500

    Initial commit

---
+++ index.js
@@ -0,0 +1,15 @@
+// TODO: Include packages needed for this application (First Part: Starter Code)
+const inquirer = require('inquirer');
+const fs = require('fs');
+
+// TODO: Create an array of questions for user input (Second Part: Starter Code)
+const questions = [];
+
+// TODO: Create a function to write README file (3. Starter Code )
+function writeToFile(fileName, data) {}
+
+// TODO: Create a function to initialize app (4. Starter Code)   
+function init() {}
+
+// Function call to initialize app (5. Starter Code)   
+init();
